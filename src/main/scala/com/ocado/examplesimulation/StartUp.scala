44450c0508c4825576693330816001013d20d070
c.brett <c.brett@ocado.com>
2016-01-26 10:56:09 +0000

    added support for getting Akka to block on tell or ask via system ExecutionContext

--- src/main/scala/com/ocado/examplesimulation/StartUp.scala
+++ src/main/scala/com/ocado/examplesimulation/StartUp.scala
@@ -1,17 +1,18 @@
 package com.ocado.examplesimulation
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{ActorSystem, Props}
-import com.ocado.event.scheduling.{ExecutorEventScheduler, SimpleDiscreteEventScheduler}
+import akka.util.Timeout
+import com.ocado.event.scheduling.{AkkaDiscreteEventScheduler, ExecutorEventScheduler, SimpleDiscreteEventScheduler}
 import com.ocado.examplecontroller.externalapi.outward.ControllerToMechanismApi
 import com.ocado.examplecontroller.{AkkaBasedController, SimpleController}
-import com.ocado.examplesimulation.AkkaSchedulerMode.AkkaSchedulerMode
-import com.ocado.examplesimulation.ControllerMode.ControllerMode
-import com.ocado.examplesimulation.CoreSchedulerMode.CoreSchedulerMode
 import com.ocado.examplesimulation.controllerapiabstraction.{AkkaBasedSimulationToControllerApi, SimpleSimulationToControllerApi}
 import com.ocado.javautils.Runnables.toRunnable
 import com.ocado.time.{AdjustableTimeProvider, UtcTimeProvider}
 import com.typesafe.config.ConfigFactory
-import com.ocado.event.scheduling.AkkaDiscreteEventScheduler
+
+import scala.concurrent.ExecutionContext
 
 object CoreSchedulerMode extends Enumeration {
   type CoreSchedulerMode = Value
@@ -28,11 +29,23 @@ object AkkaSchedulerMode extends Enumeration {
   val Default, AkkaDiscreteEventScheduler = Value
 }
 
+object AkkaExecutionContextMode extends Enumeration {
+  type AkkaSchedulerMode = Value
+  val Default, Blocking = Value
+}
+
 object StartUp extends App {
   val coreSchedulerMode = CoreSchedulerMode.withName(args(0))
+
   val controllerMode = ControllerMode.withName(args(1))
+
   val akkaSchedulerMode = controllerMode match {
     case ControllerMode.AkkaController => Option(AkkaSchedulerMode.withName(args(2)))
+    case other => Option.empty
+  }
+
+  val akkaExecutionContextMode = controllerMode match {
+    case ControllerMode.AkkaController => Option(AkkaExecutionContextMode.withName(args(3)))
     case other => Option.empty
   }
 
@@ -50,14 +63,27 @@ object StartUp extends App {
 
   def createController() = controllerMode match {
     case ControllerMode.AkkaController =>
-      val actorSystem = akkaSchedulerMode.getOrElse(AkkaSchedulerMode.Default) match {
-        case AkkaSchedulerMode.Default => ActorSystem.create()
+      implicit val timeout = Timeout(1, TimeUnit.MINUTES)
+
+      val config = akkaSchedulerMode.getOrElse(AkkaSchedulerMode.Default) match {
+        case AkkaSchedulerMode.Default => None
         case AkkaSchedulerMode.AkkaDiscreteEventScheduler =>
-          val config = ConfigFactory
-            .parseString(s"akka.scheduler.implementation=${classOf[AkkaDiscreteEventScheduler].getName}")
-            .withFallback(ConfigFactory.load())
-          ActorSystem.create("ControllerActorSystem", config)
+          Option(ConfigFactory.parseString(s"akka.scheduler.implementation=${classOf[AkkaDiscreteEventScheduler].getName}")
+            .withFallback(ConfigFactory.load()))
       }
+
+      val executionContextOption: Option[ExecutionContext] = akkaExecutionContextMode.getOrElse(AkkaExecutionContextMode.Default) match {
+        case AkkaExecutionContextMode.Default => None
+        case AkkaExecutionContextMode.Blocking =>
+          implicit val executionContext = new ExecutionContext {
+            override def reportFailure(cause: Throwable): Unit = throw cause
+
+            override def execute(runnable: Runnable): Unit = runnable.run()
+          }
+          Option(executionContext)
+      }
+
+      val actorSystem = ActorSystem("ControllerActorSystem", config, None, executionContextOption)
 
       val controller = actorSystem.actorOf(Props(new AkkaBasedController(toMechanismApi)), "Controller")
 
